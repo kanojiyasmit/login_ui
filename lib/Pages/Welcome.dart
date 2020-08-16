a1aab2b3feea432cbfd87655a78e721c77a06694
Smit <kanojiya23smit@gmail.com>
2020-08-16 22:06:43 +0530

    Add

---
+++ lib/Pages/Welcome.dart
@@ -0,0 +1,92 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:login_ui/Pages/Constant.dart';
+
+class Welcome extends StatefulWidget {
+  @override
+  _WelcomeState createState() => _WelcomeState();
+}
+
+class _WelcomeState extends State<Welcome> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [
+                  Colors.blue,
+                  Colors.blueAccent,
+                ],
+              ),
+            ),
+          ),
+          Container(
+            child: SingleChildScrollView(
+              padding: EdgeInsets.symmetric(horizontal:40,vertical: 120),
+              child: Column(
+                children: [
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                    ),
+                    child: RaisedButton(
+                      elevation: 5.0,
+                      onPressed: (){
+                        print('Sign IN');
+                      },
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                      color: Colors.white,
+                      child: Text(
+                        'SIGN IN',
+                        style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                            fontSize: 20.0,
+                            letterSpacing: 1.0,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                    ),
+                    child: RaisedButton(
+                      elevation: 5.0,
+                      onPressed: (){
+                        print('Sign Up');
+                      },
+                      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30)),
+                      color: Colors.white,
+                      child: Text(
+                        'SIGN UP',
+                        style: GoogleFonts.openSans(
+                          textStyle: TextStyle(
+                            fontSize: 20.0,
+                            letterSpacing: 1.0,
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
